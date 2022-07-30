f3d98f8a3b2c25bc406b662dcc43b0b9f6bf260c
EnotShow <88674695+EnotShow@users.noreply.github.com>
2022-07-31 01:31:35 +0300

    add tests
    added tests

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,31 @@
 import pytest
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def _add_user():
+    """ adding a user for test tasks """
+    user = User(username='admin', email='admin@gmail.com')
+    user.set_password('admin')
+    user.save()
+    return user
 
 
 @pytest.mark.django_db
 def test_my_user():
+    _add_user()
+
     me = User.objects.get(username='admin')
-    assert me.is_superuser
+    assert me
+
+
+@pytest.mark.django_db
+def test_user_auth():
+    user = _add_user()
+
+    client = APIClient()
+    refresh = RefreshToken.for_user(user)
+    client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+
+    assert client
